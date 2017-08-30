06e78eb7f6197b0d947449337ef72832aabcb037
akshatdb <akshatd4@gmail.com>
2017-08-30 22:27:45 +0530

    Removed trash

--- app/mailers/feedback_mailer.rb
+++
@@ -1,6 +0,0 @@
-class FeedbackMailer < ApplicationMailer
-	default from: "akshatd4@gmail.com"
-	def sample_email(user)
-    	@user = user
-    	mail(to: @user.email, subject: 'Sample Email')
-end
--- app/mailers/previews/feedback_mailer_preview.rb
+++
@@ -1,5 +0,0 @@
-class FeedbackMailerPreview < ActionMailer::Preview
-  def sample_mail_preview
-    FeedbackMailer.sample_email(User.first)
-  end
-end
