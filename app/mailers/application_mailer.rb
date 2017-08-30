e3bfc2977845bfd12b398d4cb2fabbb0eeae6aca
akshatdb <akshatd4@gmail.com>
2017-08-30 22:41:28 +0530

    panic mode on

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,2 @@
 class ApplicationMailer < ActionMailer::Base
-	default from: "akshatd4@gmail.com"
-	def sample_email(user)
-    	@user = user
-    	mail(to: @user.email, subject: 'Sample Email')
 end
